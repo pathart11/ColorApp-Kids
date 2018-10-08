00630bfee00902c8ce3d3fca3c46e7c39019d496
patrick.avalere <patrick.lockhart@avalere.com>
2018-10-08 17:26:14 -0400

    new app folder

---
+++ src/com/kidsgames/menu/FixedSpeedScroller.java
@@ -0,0 +1,43 @@
+package com.kidsgames.menu;
+
+
+import android.content.Context;
+import android.view.animation.Interpolator;
+import android.widget.Scroller;
+
+public class FixedSpeedScroller extends Scroller {
+
+    private int mDuration = 1;
+    private double mScrollFactor = 10;
+
+
+    public FixedSpeedScroller(Context context, Interpolator interpolator) {
+        super(context, interpolator);
+    }
+
+    @Override
+    public void startScroll(int startX, int startY, int dx, int dy) {
+        // Ignore received duration, use fixed one instead
+        super.startScroll(startX, startY, dx, dy, mDuration);
+    }
+
+    public void setScrollDurationFactor(double scrollFactor) {
+        mScrollFactor = scrollFactor;
+    }
+
+
+    @Override
+    public void startScroll(int startX, int startY, int dx, int dy,
+                            int duration) {
+
+        int newDur = duration;
+        int scrollFactor = 2;
+
+        if (duration == 200) {
+            newDur = 200;
+            scrollFactor = 5;
+        }
+        super.startScroll(startX, startY, dx, dy,
+                (int) (newDur * scrollFactor));
+    }
+}
---
+++ src/com/kidsgames/utils/ResourceId.java
@@ -0,0 +1,41 @@
+package com.kidsgames.utils;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import android.content.Context;
+
+public class ResourceId {
+    public static String DRAWABLE = "drawable";
+
+    public static int getResId(String variableName, String type, Context context) {
+        return context.getResources().getIdentifier(variableName,
+                type, context.getPackageName());
+    }
+
+    public static String getJson(Context context, String packName) {
+        String str = "";
+        try {
+            StringBuilder buf = new StringBuilder();
+            InputStream json = null;
+            if (packName.equals("pack1"))
+                json = context.getAssets().open("logic1.txt");
+            else
+                json = context.getAssets().open("logic2.txt");
+
+            BufferedReader in =
+                    new BufferedReader(new InputStreamReader(json));
+
+            while ((str = in.readLine()) != null) {
+                buf.append(str);
+            }
+            in.close();
+            return buf.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
